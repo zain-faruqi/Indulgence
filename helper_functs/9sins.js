36fcdfe537ba1d5f23936855ce1555ab71171297
Kei-KC <kyc355@nyu.edu>
2022-11-07 16:35:59 -0500

    skeleton of new categories

---
+++ helper_functs/9sins.js
@@ -0,0 +1,65 @@
+// CONTAINS ALL CATEGORY_ID & HIERARCHY DATA FOR PLAID API SORTED BY SIN
+// SINS ARE BASED OFF OF DANTES INFERNO
+
+// 1. LIMBO
+// people who lived ethically and morally but were never baptized
+const limbo = {
+    
+};
+
+// 2. LUST
+// those who committed adultery
+const lust = {
+
+};
+
+// 3. GLUTTONY
+// indulging in food or drink excessively
+// excessive purchases
+const gluttony = {
+
+};
+
+// 4. GREED & PRODIGALITY
+// reserved for people who hoarded or squandered their money
+// strove to hold as much material wealth as possible
+const greed = {
+
+};
+
+// 5. WRATH & SULLENNESS
+// those who became easily or violently angered
+// those who were sullen, or moody and grumpy, in life
+const wrath = {
+
+};
+
+// 6. HERESY
+// Rejection of religious and/or political “norms"
+// holding beliefs contrary to those taught by orthodox theologies such as Christianity
+const heresy = {
+
+};
+
+// 7. VIOLENCE
+// violence against others through murder, beatings, or destruction of property
+// violence against themselves
+const violence = {
+
+};
+
+// 8. FRAUD 
+// those who consciously and willingly commit fraud
+// Panderers and seducers, flatterers
+// sorcerers and false prophets, liars, thieves
+const fraud = {
+
+};
+
+// 9. TREACHERY
+// Betrayers of special relationships
+const treachery = {
+
+};
+
+module.exports = { limbo, lust, gluttony, greed, wrath, heresy, violence, fraud, treachery };
