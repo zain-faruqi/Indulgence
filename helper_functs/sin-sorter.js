2e66b3450ba418bc5d8e5b8cb9ad4e342c0afd77
Kei-KC <kyc355@nyu.edu>
2022-11-21 20:51:25 -0500

    make_svg function added

--- helper_functs/sin-sorter.js
+++ helper_functs/sin-sorter.js
@@ -153,76 +153,77 @@ function arbiter(API_obj) {
     return res;
 }
 
-// MAKE SVG FILE SCRIPT
-let res = arbiter(ex);
-console.log(res);
+function make_svg(transac_obj) {
+  // MAKE SVG FILE SCRIPT
+  //let res = arbiter(ex);
+  //console.log(res);
 
-// set the dimensions and margins of the graph
-var width = 500
-    height = 500
-    margin = 70
+  // set the dimensions and margins of the graph
+  var width = 500
+  height = 500
+  margin = 70
 
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-var radius = Math.min(width, height) / 2 - margin
+  // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+  var radius = Math.min(width, height) / 2 - margin
 
-// append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#my_dataviz")
-  .append("svg")
+  // append the svg object to the div called 'my_dataviz'
+  var svg = d3.select("#my_dataviz")
+    .append("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-let data = {};
-for (const [key, value] of Object.entries(res)) {
-    if (typeof(value) === 'object' && value.percent != 0)
-        data[key] = value.percent;
-}
-console.log(data);
+  let data = {};
+  for (const [key, value] of Object.entries(res)) {
+    if (typeof (value) === 'object' && value.percent != 0)
+      data[key] = value.percent;
+  }
+  console.log(data);
 
-// set the color scale
-var color = d3.scaleOrdinal()
-  .domain(Object.keys(data))
-  .range(d3.schemeRdBu[10]);
+  // set the color scale
+  var color = d3.scaleOrdinal()
+    .domain(Object.keys(data))
+    .range(d3.schemeRdBu[10]);
 
-// Compute the position of each group on the pie:
-var pie = d3.pie()
-  .sort(null) // Do not sort group by size
-  .value(function(d) {return d.value; })
-var data_ready = pie(d3.entries(data))
+  // Compute the position of each group on the pie:
+  var pie = d3.pie()
+    .sort(null) // Do not sort group by size
+    .value(function (d) { return d.value; })
+  var data_ready = pie(d3.entries(data))
 
-// The arc generator
-var arc = d3.arc()
-  .innerRadius(radius * 0.5)         // This is the size of the donut hole
-  .outerRadius(radius * 0.8)
+  // The arc generator
+  var arc = d3.arc()
+    .innerRadius(radius * 0.5)         // This is the size of the donut hole
+    .outerRadius(radius * 0.8)
 
-// Another arc that won't be drawn. Just for labels positioning
-var outerArc = d3.arc()
-  .innerRadius(radius * 0.9)
-  .outerRadius(radius * 0.9)
+  // Another arc that won't be drawn. Just for labels positioning
+  var outerArc = d3.arc()
+    .innerRadius(radius * 0.9)
+    .outerRadius(radius * 0.9)
 
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('allSlices')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', arc)
-  .attr('fill', function(d){ return(color(d.data.key)) })
-  .attr("stroke", "white")
-  .style("stroke-width", "2px")
-  .style("opacity", 0.7)
+  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+  svg
+    .selectAll('allSlices')
+    .data(data_ready)
+    .enter()
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', function (d) { return (color(d.data.key)) })
+    .attr("stroke", "white")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7)
 
-// Add the polylines between chart and labels:
-svg
-  .selectAll('allPolylines')
-  .data(data_ready)
-  .enter()
-  .append('polyline')
+  // Add the polylines between chart and labels:
+  svg
+    .selectAll('allPolylines')
+    .data(data_ready)
+    .enter()
+    .append('polyline')
     .attr("stroke", "black")
     .style("fill", "none")
     .attr("stroke-width", 1)
-    .attr('points', function(d) {
+    .attr('points', function (d) {
       var posA = arc.centroid(d) // line insertion in the slice
       var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
       var posC = outerArc.centroid(d); // Label position = almost the same as posB
@@ -231,20 +232,22 @@ svg
       return [posA, posB, posC]
     })
 
-// Add the polylines between chart and labels:
-svg
-  .selectAll('allLabels')
-  .data(data_ready)
-  .enter()
-  .append('text')
-    .text( function(d) { console.log(d.data.key) ; return d.data.key } )
-    .attr('transform', function(d) {
-        var pos = outerArc.centroid(d);
-        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-        pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-        return 'translate(' + pos + ')';
+  // Add the polylines between chart and labels:
+  svg
+    .selectAll('allLabels')
+    .data(data_ready)
+    .enter()
+    .append('text')
+    .text(function (d) { console.log(d.data.key); return d.data.key })
+    .attr('transform', function (d) {
+      var pos = outerArc.centroid(d);
+      var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+      pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+      return 'translate(' + pos + ')';
     })
-    .style('text-anchor', function(d) {
-        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-        return (midangle < Math.PI ? 'start' : 'end')
+    .style('text-anchor', function (d) {
+      var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+      return (midangle < Math.PI ? 'start' : 'end')
     })
+}
+
