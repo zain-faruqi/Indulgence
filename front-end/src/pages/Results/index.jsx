97033b26c04a53ff55a72e69e99a0d47e0466f38
Zain Faruqi <zf698@nyu.edu>
2022-11-22 10:43:07 -0800

    weak sharing implementation

--- front-end/src/pages/Results/index.jsx
+++ front-end/src/pages/Results/index.jsx
@@ -5,6 +5,9 @@ import greed from '../../img/greed.png';
 import gluttony from '../../img/gluttony.png';
 import heresy from '../../img/heresy.png';
 import './styles.css';
+import { saveAs } from 'file-saver';
+import Button from 'react-bootstrap/Button'
+import html2canvas from 'html2canvas';
 
 
 
@@ -14,10 +17,23 @@ function Results() {
   const [index, setIndex] = useState(Math.floor(Math.random() * 4));
   const [sins, setSins] = useState(["Wrath", "Greed", "Gluttony", "Heresy"]);
 
+  const downloadImage = () => {
+    
+    const div = document.getElementById('hell');
+    html2canvas(div)
+      .then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+        window.open(imgData);
+      })
+    
+  }; 
+  
 
+  
       
     return (
-      <div className='Results'>
+      <div className='Results' >
+        <div id="hell">
         <h2>Enjoy your time in Hell!</h2>
         <p>
           <img src={images[parseInt(index)]} alt="hell image" height={400} width={400} />
@@ -26,6 +42,10 @@ function Results() {
         <p>
           You are going to hell for {sins[parseInt(index)]}
         </p>
+          
+          
+      </div>
+        <button onClick={downloadImage} variant="danger">share</button>
       </div>
     )
 
