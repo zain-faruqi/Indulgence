8b7a5a69193ddc53863203206644ddf23a777bfb
Kei-KC <kyc355@nyu.edu>
2022-12-05 20:12:47 -0500

    Create ChartComponent.js

---
+++ front-end/src/components/donut_chart/ChartComponent.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef } from "react";
+import drawChart from "./drawCHart";
+
+const DonutChart = ({ data }) => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if (ref.current) {
+            drawChart(ref.current, data);
+        }
+    }, [ref]);
+
+    return (
+        <div className="container">
+            <div className="graph" ref={ref} />
+        </div>
+    );
+};
+
+export default React.memo(DonutChart);
