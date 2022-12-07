b07605fb776391d640b21942ac6b407892e6e2f3
Zain Faruqi <zf698@nyu.edu>
2022-12-07 16:23:47 -0500

    share

--- front-end/src/pages/Results/index.jsx
+++ front-end/src/pages/Results/index.jsx
@@ -4,97 +4,26 @@ import wrath from '../../img/wrath.png';
 import greed from '../../img/greed.png';
 import gluttony from '../../img/gluttony.png';
 import heresy from '../../img/heresy.png';
+import limbo from '../../img/limbo.png';
+import treachery from '../../img/treachery.png';
+import lust from '../../img/lust.png';
 import './styles.css';
-import { saveAs } from 'file-saver';
 import Button from 'react-bootstrap/Button'
-import html2canvas from 'html2canvas';
-//import ChartComponent from '../../components/donut_chart/ChartComponent.js';
 
-const baseURL = '/api/transactions';
 
-/*
-React.useEffect(() => {
-  const createLinkToken = async () => {
-    axios.get('/api/create_link_token')
-      .then(function (response) {
-        console.log(response);
-        const link_token = response.data.link_token;
-        setToken(link_token);
-        console.log("token: " + token);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  createLinkToken();
-}, []);
-*/
-
-/*
-export default function Results() {
-  var transactions = [];
-  console.log("1")
-  const getTransactions = async () => {
-    axios.get('/api/transactions').then((response) => {
-      consolf.log("2")
-      console.log(response);
-      transactions = response.data.transactions;
-    });
-  }
-
-  getTransactions();
-*/
-
-  /*
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    const getTransactions = async () => {
-      axios.get('/api/transactions').then((response) => {
-        console.log(response);
-        setPost(response.data);
-      });
-    }
-
-    getTransactions();
-  }, []);
-  */
-  /*if (!post) return null;*/
-
-/*
-  return (
-    <div>
-      {transactions}
-    </div>
-  );
-}
-*/
 
 export default function Results() {
   
   const [images, setImages] = useState([wrath, greed, gluttony, heresy, limbo, heresy, treachery, lust]);
   const [sins, setSins] = useState(["wrath", "greed", "gluttony", "heresy", "limbo", "heresy", "treachery", "lust"]);
 
-  const downloadImage = () => {
-    
-    const div = document.getElementById('hell');
-    html2canvas(div)
-      .then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-        window.open(imgData);
-      })
-    
-  }; 
 
   const [transactions, setTransactions] = useState([]);
 
-  // DATA FOR DONUT CHART
-  // const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     (async () => {
       await getTransactions();
-      //await setChartData();
     })();
 },[]);
   
@@ -105,28 +34,8 @@ export default function Results() {
            axios.get('/api/transactions')
           .then(function (response) {
               console.log('response:' + JSON.stringify(response.data));
-              //type error
               setTransactions(response.data);
-              //let transactions = response.data;
               console.log(transactions);
-              //return transactions;
-
-              // populate setChartData using transactions obj
-              // includes labels
-              // data = [ 
-              //    { sin: 'limbo', value: 10 }, 
-              //    { sin: 'lust', value: 2 },
-              // ]
-              /*
-              let data = [];
-              for (const [key, val] of Object.entries(transactions)) {
-              if (typeof (val) === 'object' && val.percent != 0)
-                data.push({sin: key, value: val.percent});
-              }
-              console.log(data)
-              setChartData(data);
-              console.log(chartData);
-              */
           })
           .catch((error) => {
             console.error(error);
@@ -140,7 +49,6 @@ export default function Results() {
       }
     );
   }
-  //getTransactions();
 
   const sin = transactions["sin"];
   const index = sins.indexOf(sin);
@@ -157,6 +65,19 @@ export default function Results() {
   } else {
     sinText = "You are going to hell for " + sin;
   }
+
+  const share = () => {
+    console.log("share");
+    if (navigator.share) {
+      navigator.share({
+        title: 'Your Hell',
+        text: `I am going to hell for ${sin}!}`,
+        url: images[parseInt(index)],
+      })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    }
+  }
   
   return (
   
@@ -172,122 +93,10 @@ export default function Results() {
         <p>
           Your Total number of transaction is {transactions["transac_ct"]}
         </p>
-      
+        <Button variant="danger" onClick={() => share()} >share</Button>
       </div>
     </div>
   )
 
 }
 
-/*<div>{tramsaction}</div> */
-/*
-  const [images, setImages] = useState([wrath, greed, gluttony, heresy]);
-  const [index, setIndex] = useState(Math.floor(Math.random() * 4));
-  const [sins, setSins] = useState(["Wrath", "Greed", "Gluttony", "Heresy"]);
-
-  const downloadImage = () => {
-    
-    const div = document.getElementById('hell');
-    html2canvas(div)
-      .then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-        window.open(imgData);
-      })
-    
-  }; 
-  
-
-  
-      
-
-    return (
-      <div className='Results' >
-        <div id="hell">
-        <h2>Enjoy your time in Hell!</h2>
-        <p>
-          <img src={images[parseInt(index)]} alt="hell image" height={400} width={400} />
-        </p>
-
-        <p>
-          You are going to hell for {sins[parseInt(index)]}
-        </p>
-          
-          
-      </div>
-        <button onClick={downloadImage} variant="danger">share</button>
-      </div>
-    )
-
-}
-
-export default Results;
-*/  
-    
-/*
-   return (
-      <div>
-      {getTransactions}
-        Returned
-      </div>
-    )
-
-}
-*/
-
-
-/*
-export default class Results extends React.Component {
-  state = {
-    transactions: []
-  }
-
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
-      })
-  }
-
-  render() {
-    return (
-      <ul>
-        {
-          this.state.persons
-            .map(person =>
-              <li key={person.id}>{person.name}</li>
-            )
-        }
-      </ul>
-    )
-  }
-}
-*/
-/*export default class Results extends React.Component{
-  async getData() {
-      const res = await axios("/api/transactions");
-      return await res.json(); // (Or whatever)
-  }
-  constructor(...args) {
-      super(...args);
-      this.state = {data: null};
-  }
-  componentDidMount() {
-      if (!this.state.data) {
-          (async () => {
-              try {
-                  this.setState({data: await this.getData()});
-              } catch (error) {
-                  console.log("U got an error!")
-              }
-          })();
-      }
-  }
-  render() {
-      return (
-          <div>
-              {this.state.data ? <em>Loading...</em> : this.state.data}
-          </div>
-      );
-  }
-}*/
